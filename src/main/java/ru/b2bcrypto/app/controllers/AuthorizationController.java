26f55a2a6224d79d000f769cbb67d197461be89a
rafailvv <rafail.02@mail.ru>
2024-02-22 19:45:10 +0300

    update controller

--- src/main/java/ru/b2bcrypto/app/controllers/AuthorizationController.java
+++ src/main/java/ru/b2bcrypto/app/controllers/AuthorizationController.java
@@ -1,16 +1,18 @@
 package ru.b2bcrypto.app.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping
 public class AuthorizationController {
-    @GetMapping(value = "/")
-    public String registrationPage(Model model) {
-        System.out.println("1");
-        return "index";
+    @GetMapping(value = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String welcomeAsHTML() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
     }
 }
